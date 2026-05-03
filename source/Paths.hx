a0b3191874d44dadcebdda63a746869bf95ba059
ikengjj <manuelsteve893@gmail.com>
2026-05-03 15:51:42 -0500

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -222,6 +222,9 @@ class Paths
 
 	inline static public function voices(song:String):Any
 	{
+		if(songKey == 'sonibars') {
+			return null;
+		}
 		var songKey:String = '${song.toLowerCase().replace(' ', '-')}/Voices';
 		var voices = returnSound('songs', songKey);
 		return voices;
